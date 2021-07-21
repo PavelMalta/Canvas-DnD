924d7bf116d852ef508ed4eaf652a05a7533e5a9
PavelMalta <shamashov87@mail.ru>
2021-07-21 21:21:46 +0300

    added the logic for saving data to the localStorage

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,13 +1,20 @@
 import {combineReducers, createStore} from "redux";
 import {figuresReducer} from "./figures-reducer";
+import {loadState, saveState} from "./utils/localStorage-utils";
 
 const rootReducer = combineReducers({
     figures: figuresReducer,
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, loadState())
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
+store.subscribe( () => {
+    saveState({
+        figures: store.getState().figures
+    })
+})
 
 // @ts-ignore
 window.store = store;
